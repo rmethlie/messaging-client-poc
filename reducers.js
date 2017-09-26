ae4346e59d9a92a7894aed5cb93863ecb34fd35e
Russ Methlie <russ.methlie@thomsonreuters.com>
2017-09-26 16:38:52 -0400

    working container tree

--- reducers.js
+++ reducers.js
@@ -1,7 +1,37 @@
+const STATUS_LABELS = [
+  'INITIAL',
+  'CONNECTING',
+  'CONFAIL',
+  'AUTHENTICATING',
+  'AUTHFAIL',
+  'CONNECTED',
+  'DISCONNECTED',
+  'DISCONNECTING',
+  'FATAL_FAILURE',
+  'REDIRECT',
+  'CONNTIMEOUT'
+];
 
 const connectionMiddleware = ({ getState }) => {
   let connection;
   return (next) => {
+    function onConnectionStatus({ status }) {
+      next({
+        type: 'SET_CONNECTION_STATUS',
+        data: STATUS_LABELS[status]
+      });
+      // TODO: Not here...
+      if (status === 5) {
+        next({
+          type: 'SET_AUTH_STATUS',
+          data: 'AUTHENTICATED'
+        });
+      }
+    }
+    function normalizeCredentials(creds) {
+      // TODO: normalize jid
+      return creds;
+    }
     return (action) => {
       switch (action.type) {
         case 'CONNECT':
@@ -10,10 +40,11 @@ const connectionMiddleware = ({ getState }) => {
             appVersion: '0.0.1',
             clientRevision: '12345'
           });
-          connection.connect({
-            jid: 'russ3.methlie.thomsonreuters.com@reuasmb.net',
-            password: 'Welcome1'
-          });
+          connection.connect(
+            Object.assign(normalizeCredentials(action.data), {
+              callback: onConnectionStatus
+            })
+          );
           break;
         default: next(action);
       }
@@ -21,14 +52,13 @@ const connectionMiddleware = ({ getState }) => {
   }
 }
 
-
-
 const defaultConnectionState = {
   status: 'disconnected'
 };
+
 const connection = (state = defaultConnectionState, action = {}) => {
   switch (action.type) {
-    case 'STATUS':
+    case 'SET_CONNECTION_STATUS':
       return Object.assign({}, state, {
         status: action.data
       });
@@ -40,11 +70,23 @@ const authenticationMiddleware = ({ dispatch, getState }) => {
   return (next) => {
     return (action) => {
       switch (action.type) {
-        case 'AUTHENTICATE_SSO':
+        case 'AUTHENTICATE_SIMPLE':
           dispatch({
-            type: 'CONNECT'
-          })
+            type: 'CONNECT',
+            data: action.data
+          });
           break;
+        case 'AUTHENTICATE_SSO': {
+          next({
+            type: 'SET_AUTH_STATUS',
+            data: 'AUTHENTICATING'
+          });
+          dispatch({
+            type: 'CONNECT',
+            data: action.data
+          });
+          break;
+        }
         default: next(action);
       }
     }
@@ -52,9 +94,15 @@ const authenticationMiddleware = ({ dispatch, getState }) => {
 }
 
 const defaultAuthenticationState = {
-  type: 'simple',
-  status: 'pending'
+  type: 'unset',
+  status: 'pending',
+  config: {
+    sso: {
+      url: `/collab-cm/performSSOV2.htm`
+    }
+  }
 };
+
 const authentication = (state = defaultAuthenticationState, action = {}) => {
   switch (action.type) {
     case 'SET_AUTH_TYPE':
@@ -63,8 +111,60 @@ const authentication = (state = defaultAuthenticationState, action = {}) => {
       });
     case 'SET_AUTH_STATUS':
       return Object.assign({}, state, {
-        status: 'authenticating'
+        status: action.data
       })
     default: return state;
+  }
+}
+
+const containerTree = new TreeModel();
+const defaultContainerState = {
+  _id: '0',
+  title: 'Untitled Container'
+}
+const defaultContainersState = Object.assign({}, defaultContainerState, {
+  title: 'Root Container'
+});
+console.log('defaultContainersState', defaultContainersState);
+const containersRoot = containerTree.parse(defaultContainersState);
+
+function createContainer(config) {
+  return containerTree.parse({
+    _id: config.id,
+    title: config.title
+  });
+}
+
+function addContainer(state, action) {
+  const containerNode = createContainer(action.data);
+  // should we add to some other child or the root?
+  const parent = action.data.parent ?
+    containersRoot.first((node) => {
+      return node.model._id === action.data.parent
+    }) :
+    containersRoot;
+  // add new child to parent
+  parent.addChild(containerNode);
+  // return the new containers object
+  return containersRoot;
+}
+
+function removeContainer(state, action) {
+  const containerId = action.data.id;
+  let containers = Object.assign({}, state.containers);
+  delete containers.containerId;
+  return Object.assign({}, state, containers);
+}
+
+const containers = (state = containersRoot, action = {}) => {
+  switch (action.type) {
+    case 'ADD_CONTAINER':
+      return addContainer(state, action);
+      break;
+    case 'REMOVE_CONTAINER':
+      return removeContainer(state, action);
+      break;
+    default:
+      return state;
   }
 }
